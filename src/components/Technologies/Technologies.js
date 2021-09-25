cd16af2c9d94fb922e885e9f3cfb2c9be72a9e51
JuanR-T <juan.m13@hotmail;fr>
2021-09-25 05:05:50 +0200

    Skills

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SiAdobexd, SiAdobephotoshop, SiAdobeillustrator, SiHtml5, SiCss3, SiPhp, SiReact, SiJavascript } from 'react-icons/si';
+import {SiMysql, SiAdobexd, SiAdobephotoshop, SiAdobeillustrator, SiHtml5, SiCss3, SiPhp, SiReact, SiJavascript } from 'react-icons/si';
 import {DiReact, DiNodejsSmall} from 'react-icons/di';
 import {FiFigma, FiDatabase} from 'react-icons/fi';
 import {FaNodeJs} from 'react-icons/fa';
@@ -32,7 +32,7 @@ const Technologies = () =>  (
           <ListTitle>Front-End</ListTitle>
           <ListParagraph>
             Connaissances en <br/>
-            Html, Css, Php, React.js
+            Html, Css, Sass, Php, React.js, Next.js
           </ListParagraph>
           
         </ListContainer>
@@ -43,13 +43,15 @@ const Technologies = () =>  (
         <FiDatabase size="3rem" />
         &nbsp;
         <FaNodeJs size="3rem" />
+        &nbsp;
+        <SiMysql size="4rem" />
         </div>
         <br/>
         <ListContainer>
           <ListTitle>Back-End</ListTitle>
           <ListParagraph>
             Connaissances en <br/>
-            Node.js et bases de données
+            Node.js, MySQL
           </ListParagraph>
         </ListContainer>
       </ListItem>
